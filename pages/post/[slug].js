130577ac70eeac7316fc3b028351e76bf12d3431
Cory Buecker <cory.buecker@gmail.com>
2019-12-20 07:43:03 -0600

    Fix dynamic link

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -6,38 +6,44 @@ import Head from 'next/head'
 import '../../stylesheets/minima-classic.scss'
 
 import posts from '../../src/content.json'
-
+import { useRouter } from 'next/router'
 const postsBySlug = posts.reduce((p, post) => {
   p[post.path] = post
   return p
 }, {})
 
-const PostBySlug = post => {
+const PostBySlug = () => {
   useEffect(() => {
     fetch('https://analytics.corybuecker.com')
   }, [])
 
-  return (
-    <div>
-      <Head>
-        <title>{post.attributes.title} - Cory Buecker</title>
-      </Head>
-      <Header></Header>
-      <main className="page-content" aria-label="Content">
-        <div className="wrapper">
-          <Post
-            key="1"
-            body={post.body}
-            path={post.path}
-            title={post.attributes.title}
-            published={post.attributes.published}
-          ></Post>
-        </div>
-      </main>
-    </div>
-  )
-}
+  const router = useRouter()
+  const { slug } = router.query
+  const post = postsBySlug[slug]
 
-PostBySlug.getInitialProps = async ({ query }) => postsBySlug[query.slug]
+  if (post !== undefined) {
+    return (
+      <div>
+        <Head>
+          <title>{post.attributes.title} - Cory Buecker</title>
+        </Head>
+        <Header></Header>
+        <main className="page-content" aria-label="Content">
+          <div className="wrapper">
+            <Post
+              key="1"
+              body={post.body}
+              path={post.path}
+              title={post.attributes.title}
+              published={post.attributes.published}
+            ></Post>
+          </div>
+        </main>
+      </div>
+    )
+  } else {
+    return <div></div>
+  }
+}
 
 export default PostBySlug
