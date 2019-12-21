14d4ffac45c9a842fead31ec4bec195b6c9a573f
Cory Buecker <cory.buecker@gmail.com>
2019-12-21 08:06:10 -0600

    Remove next-css

--- next.config.js
+++ next.config.js
@@ -1,27 +1,24 @@
 const content = require('./src/content.json')
 const withSass = require('@zeit/next-sass')
-const withCss = require('@zeit/next-css')
 
-module.exports = withCss(
-  withSass({
-    exportPathMap: async (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) => {
-      return content.reduce(
-        (pages, page) => {
-          pages[`/post/${page.path}`] = {
-            page: '/post/[slug]',
-            query: { slug: page.path }
-          }
-
-          return pages
-        },
-        {
-          '/index': { page: '/index' },
-          '/': { page: '/' }
+module.exports = withSass({
+  exportPathMap: async (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) => {
+    return content.reduce(
+      (pages, page) => {
+        pages[`/post/${page.path}`] = {
+          page: '/post/[slug]',
+          query: { slug: page.path }
         }
-      )
-    }
-  })
-)
+
+        return pages
+      },
+      {
+        '/index': { page: '/index' },
+        '/': { page: '/' }
+      }
+    )
+  }
+})
