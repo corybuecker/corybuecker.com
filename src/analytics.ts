447033a2726ba903377acf4dae04bae3875ba29b
Cory Buecker <cory.buecker@gmail.com>
2020-01-02 06:35:08 -0600

    Add link tracking

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,21 +1,46 @@
-const recordPageview = async () => {
+const recordPageview = (): Promise<{}> => {
   const analyticsUrl = new URL('https://analytics.corybuecker.com')
   const pageUrl = new URL(window.location.toString())
 
   analyticsUrl.search = `page=${pageUrl.pathname}`
 
-  await fetch(analyticsUrl.toString())
+  return fetch(analyticsUrl.toString())
 }
 
 recordPageview()
 
-const formatTimeElement = (element: HTMLTimeElement) => {
+const formatTimeElement = (element: HTMLTimeElement): void => {
   const timeString = element.dateTime
-  const dateTime = new Date(timeString)
 
-  element.innerText = dateTime.toLocaleDateString()
+  element.innerText = new Date(timeString).toLocaleDateString()
 }
 
 const timeElements = document.getElementsByTagName('time')
 
 Array.from(timeElements).forEach(formatTimeElement)
+
+class TrackedAnchor extends HTMLElement {
+  constructor() {
+    super()
+
+    this.addEventListener('click', this.handleTrackedAnchorClick)
+  }
+
+  handleTrackedAnchorClick(event: Event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const target = event.target as HTMLAnchorElement
+    const analyticsUrl = new URL('https://analytics.corybuecker.com')
+
+    analyticsUrl.search = `click_link=${target?.href}`
+
+    fetch(analyticsUrl.toString())
+      .then(() => (window.location.href = target?.href))
+      .catch(() => (window.location.href = target?.href))
+
+    return false
+  }
+}
+
+window.customElements.define('tracked-anchor', TrackedAnchor)
